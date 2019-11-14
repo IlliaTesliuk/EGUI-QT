253da2ef2b4129ebe247733b155c160c91808b13
Jakub Gaik <52374575+jgaik@users.noreply.github.com>
2019-11-14 15:59:20 +0100

    Add files via upload

--- notebook.cpp
+++ notebook.cpp
@@ -1,11 +1,46 @@
 #include "notebook.h"
+#include "noteedit.h"
+
+#include <QBoxLayout>
 
 NoteBook::NoteBook(QWidget *parent)
     : QMainWindow(parent)
 {
+    labelDateFrom = new QLabel("From: ");
+    labelDateTo = new QLabel("To: ");
+    labelCategory = new QLabel("Cateogry: ");
+
+    buttonFilter = new QPushButton("Filter");
+    buttonClear = new QPushButton("Clear");
+
+    QHBoxLayout *layoutFilter = new QHBoxLayout();
+    layoutFilter->addWidget(labelDateFrom);
+    layoutFilter->addWidget(dateFrom);
+    layoutFilter->addWidget(labelDateTo);
+    layoutFilter->addWidget(dateTo);
+    layoutFilter->addWidget(labelCategory);
+    layoutFilter->addWidget(comboCategory);
+    layoutFilter->addWidget(buttonFilter);
+    layoutFilter->addWidget(buttonClear);
+
+    QHBoxLayout *layoutNoteActions = new QHBoxLayout();
+    layoutNoteActions->addWidget(buttonNewNote);
+    layoutNoteActions->addWidget(buttonEditNote);
+    layoutNoteActions->addWidget(buttonDeleteNote);
+
+    QVBoxLayout *layoutMain = new QVBoxLayout();
+    layoutMain->addLayout(layoutFilter);
+    layoutMain->addWidget(treeNoteList);
+    layoutMain->addLayout(layoutNoteActions);
 }
 
 NoteBook::~NoteBook()
 {
 
+}
+
+NoteBook::openNote()
+{
+    NoteEdit note;
+    note.exec();
 }
--- notebook.h
+++ notebook.h
@@ -1,7 +1,11 @@
 #ifndef NOTEBOOK_H
 #define NOTEBOOK_H
 
+#include <QComboBox>
+#include <QLabel>
 #include <QMainWindow>
+#include <QPushButton>
+#include <QTreeView>
 
 class NoteBook : public QMainWindow
 {
@@ -10,6 +14,21 @@ class NoteBook : public QMainWindow
 public:
     NoteBook(QWidget *parent = 0);
     ~NoteBook();
+private slots:
+    void openNote();
+private:
+    QLabel *labelDateFrom;
+    QLabel *labelDateTo;
+    QLabel *labelCategory;
+    QDateTime *dateFrom;
+    QDateTime *dateTo;
+    QComboBox *comboCategory;
+    QPushButton *buttonFilter;
+    QPushButton *buttonClear;
+    QTreeView *treeNoteList;
+    QPushButton *buttonNewNote;
+    QPushButton *buttonEditNote;
+    QPushButton *buttonDeleteNote;
 };
 
 #endif // NOTEBOOK_H
